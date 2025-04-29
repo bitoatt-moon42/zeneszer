4561614c0a3031b05170a2e566803d6c4abca0d1
Csaba Danyiko <csaba.danyiko.ext@porscheinformatik.com>
2025-04-29 14:41:43 +0200

    footer stb

--- src/main/webapp/app/app.component.ts
+++ src/main/webapp/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavigationStart, Router, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from 'app/common/header/header.component';
+import {FooterComponent} from 'app/common/footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, RouterOutlet, HeaderComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
